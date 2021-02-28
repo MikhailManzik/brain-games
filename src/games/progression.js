9467c9211c07d538d5f0e82a21947a08dbf815f1
mikhail manzik <mikhailmanzik@yandex.ru>
2021-02-28 22:55:45 +0200

    Выносит длину прогресси в глобальную переменную.

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,25 +1,24 @@
 import runGame from '../index.js';
 import { getRandomNumber } from '../utils.js';
 
+const progressionLength = 10;
 const description = 'What number is missing in the progression?';
 
 const getArithmeticProgression = () => {
   let progressionNumber = getRandomNumber(0, 50);
   const step = getRandomNumber(2, 5);
-  let progressionLength = 0;
   const progression = [];
 
-  while (progressionLength < 10) {
+  for (let i = 0; i < progressionLength; i += 1) {
     progression.push(progressionNumber);
     progressionNumber += step;
-    progressionLength += 1;
   }
   return progression;
 };
 
 const getQuestionAndAnswer = () => {
   const progression = getArithmeticProgression();
-  const randomIndex = getRandomNumber(0, 9);
+  const randomIndex = getRandomNumber(0, progressionLength);
 
   const correctAnswer = String(progression[randomIndex]);
   progression[randomIndex] = '..';
